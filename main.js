44a3428f684c90a09811bf0e4376acc34ba17bab
Dan Actor <danltactor@gmail.com>
2018-02-25 21:54:39 -0700

    complete basic

---
+++ main.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+setDate = () => {
+  const now = new Date();
+  // seconds
+  const second = now.getSeconds();
+  const secondDegrees = ((second / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  // minutes
+  const minute = now.getMinutes();
+  const minuteDegrees = ((minute / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minuteDegrees}deg)`; 
+  // hours
+  const hour = now.getMinutes();
+  const hourDegrees = ((hour / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`
+}
+
+setInterval(setDate, 1000);
